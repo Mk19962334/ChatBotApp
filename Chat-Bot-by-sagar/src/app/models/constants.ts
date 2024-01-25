38ccfd9858f65ace82ad75b9ddf60c5f1c56c34b
Sagar20091 <sagargopale2020@gmail.com>
2024-01-25 18:35:13 +0530

    "Adding Chat-bot-App"

---
+++ Chat-Bot-by-sagar/src/app/models/constants.ts
@@ -0,0 +1,22 @@
+export const gptModels = [
+    {
+        'model':'gpt-3.5-turbo-instruct',
+        'description':'Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.',
+        'maxRequest':'4,000 tokens'
+    },
+    {
+        'model':'text-curie-001',
+        'description':'Very capable, but faster and lower cost than Davinci.',
+        'maxRequest':'2,048 tokens'
+    },
+    {
+        'model':'text-babbage-001',
+        'description':'Capable of straightforward tasks, very fast, and lower cost.',
+        'maxRequest':'2,048 tokens'
+    },
+    {
+        'model':'text-ada-001',
+        'description':'Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.',
+        'maxRequest':'4,000 tokens'
+    }
+]
